80b43b0e9ad41df3bd7eb2c7ea1e48433292dd85
Valeri <vgladunik@gmail.com>
2026-06-03 17:32:16 +0300

    Add featured vehicles section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue'
 import MissionSection from '../components/MissionSection.vue'
+import FeaturedVehiclesSection from '../components/FeaturedVehiclesSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <MissionSection />
+  <FeaturedVehiclesSection />
 </template>
