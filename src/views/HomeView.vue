7e0a84fb16678c7b9fdfe7ef39345e46ac6cdf4b
Valeri <vgladunik@gmail.com>
2026-06-03 17:42:20 +0300

    Add blog section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
 import HeroSection from '../components/HeroSection.vue'
 import MissionSection from '../components/MissionSection.vue'
 import FeaturedVehiclesSection from '../components/FeaturedVehiclesSection.vue'
+import BlogNewsSection from '../components/BlogNewsSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <MissionSection />
   <FeaturedVehiclesSection />
+  <BlogNewsSection />
 </template>
