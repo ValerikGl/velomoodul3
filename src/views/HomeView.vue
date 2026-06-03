38ab4decf6dbabd5fb8fb10acf4bbac2935d13b6
Valeri <vgladunik@gmail.com>
2026-06-03 16:31:35 +0300

    Add mission section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue'
+import MissionSection from '../components/MissionSection.vue'
 </script>
 
 <template>
   <HeroSection />
+  <MissionSection />
 </template>
